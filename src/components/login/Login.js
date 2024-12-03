69d6a511047e7d45fc9903e20cf4bffb5099686a
LucaMinguzzi <l.minguzzi@reply.it>
2024-12-03 13:54:32 +0100

    log-in logo fixed

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -67,7 +67,8 @@ export default function Login() {
     <div className="log-in-page">
       <div className="log-in-card">
         <div className="logo-container">
-          <img src="./images/logo.png" alt="logo" />
+          <img src="./images/logo.svg" alt="logo" />
+          <h1 className="app-title">Workbook</h1>
         </div>
         <form onSubmit={logInHandler}>
           <Field
